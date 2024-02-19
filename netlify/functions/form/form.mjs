2c927fd72c623263cc222084ad9ab5c90d13d8a8
DengelLionel <dengelriveracajincho@gmail.com>
2024-02-19 18:49:21 -0500

    a

--- netlify/functions/form/form.mjs
+++ netlify/functions/form/form.mjs
@@ -1,14 +1,11 @@
-import axios from 'axios';
+const axios = require('axios');
 
-// Asume que has configurado las variables de entorno en Netlify para SHOPIFY_API_KEY, SHOPIFY_API_PASSWORD, y SHOPIFY_SHOP_NAME
-const SHOPIFY_API_KEY = process.env.SHOPIFY_API_KEY;
-const SHOPIFY_API_PASSWORD = process.env.SHOPIFY_API_PASSWORD;
-const SHOPIFY_SHOP_NAME = process.env.SHOPIFY_SHOP_NAME;
+exports.handler = async (event, context) => {
+  const { dni, celular } = JSON.parse(event.body); // Asegúrate de que estos campos coincidan con los nombres de tus inputs en el formulario.
+  const SHOPIFY_API_KEY = process.env.SHOPIFY_API_KEY; // Configura estas variables en tu entorno serverless.
+  const SHOPIFY_API_PASSWORD = process.env.SHOPIFY_API_PASSWORD;
+  const SHOPIFY_SHOP_NAME = process.env.SHOPIFY_SHOP_NAME; // Asegúrate de incluir solo el nombre de tu tienda, sin ".myshopify.com".
 
-export default async (req, context) => {
-  // Parsea el cuerpo de la solicitud (asumiendo que es JSON)
-  const { dni, celular } = await req.json();
-console.log("shop",SHOPIFY_SHOP_NAME)
   try {
     const response = await axios.post(`https://${SHOPIFY_SHOP_NAME}.myshopify.com/admin/api/2023-01/metafields.json`, {
       metafield: {
@@ -17,7 +14,7 @@ console.log("shop",SHOPIFY_SHOP_NAME)
         value: JSON.stringify({ dni, celular }),
         value_type: 'json_string',
         owner_resource: 'product',
-        owner_id: 'el_id_del_recurso' // Asegúrate de reemplazar esto con el ID real del recurso al que quieres asociar el metafield
+        owner_id: 'el_id_del_recurso'
       }
     }, {
       auth: {
@@ -26,17 +23,15 @@ console.log("shop",SHOPIFY_SHOP_NAME)
       }
     });
 
-    // Retorna una respuesta exitosa con los datos de la respuesta de Shopify
-    return new Response(JSON.stringify(response.data), {
-      headers: { 'Content-Type': 'application/json' },
-      status: 200
-    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data)
+    };
   } catch (error) {
-    console.error('Error al actualizar metafield:', error);
-    // Retorna una respuesta de error
-    return new Response(JSON.stringify({ error: 'Failed to update metafield' }), {
-      headers: { 'Content-Type': 'application/json' },
-      status: 500
-    });
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to update metafield' })
+    };
   }
 };
