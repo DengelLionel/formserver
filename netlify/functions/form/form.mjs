0fc369bda65a7092228e290dc2767c86f8f3076c
DengelLionel <dengelriveracajincho@gmail.com>
2024-02-25 17:12:19 -0500

    ok

--- netlify/functions/form/form.mjs
+++ netlify/functions/form/form.mjs
@@ -12,7 +12,7 @@ exports.handler = async (event) => {
   const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Allow-Methods': 'POST,GET, OPTIONS',
+    'Access-Control-Allow-Methods': 'POST,GET,OPTIONS',
     'Content-Security-Policy': "default-src 'self'; script-src 'self' https://apis.google.com; object-src 'none';"
   };
 
@@ -47,22 +47,25 @@ exports.handler = async (event) => {
       documentselected,
     };
 
-    // Realizar solicitud POST al script de Google Apps Script
-    const response = await axios.post(scriptUrl, JSON.stringify(scriptData), {
+    // Realizar solicitud POST al script de Google Apps Script a través de un proxy
+    // Cambia 'tu-url-de-proxy' a la URL de tu función serverless de Netlify que actúa como proxy
+    const proxyUrl = 'https://remarkable-hummingbird-2f0c09.netlify.app/.netlify/functions/proxy'; // Esta debería ser la URL de tu función serverless diseñada para actuar como proxy
+    const response = await axios.post(proxyUrl, JSON.stringify(scriptData), {
       headers: {
         'Content-Type': 'application/json',
+        // Agrega cualquier cabecera adicional requerida por tu proxy o el destino
       },
     });
 
-    // Verifica la respuesta del script de Google Apps Script
+    // Verifica la respuesta del proxy (que a su vez es la del script de Google Apps Script)
     if (response.status === 200) {
-      console.log('Datos enviados al script de Google Apps Script con éxito:', response.data);
-      return { statusCode: 200, headers, body: 'Datos guardados correctamente y enviados al script de Google Apps Script' };
+      console.log('Datos reenviados a través del proxy y al script de Google Apps Script con éxito:', response.data);
+      return { statusCode: 200, headers, body: 'Datos guardados correctamente y reenviados a través del proxy al script de Google Apps Script' };
     } else {
-      throw new Error('Error al enviar datos al script de Google Apps Script');
+      throw new Error('Error al reenviar datos a través del proxy al script de Google Apps Script');
     }
   } catch (error) {
-    console.error('Error guardando los datos o enviando al script de Google Apps Script:', error);
+    console.error('Error guardando los datos o reenviándolos a través del proxy:', error);
     return { statusCode: 500, headers, body: 'Error interno del servidor' };
   }
 };
